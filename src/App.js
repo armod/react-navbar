08a896b376c21212743ff71a72b7c0461fc2837b
armod <armods@gmail.com>
2022-09-06 19:14:54 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Navbar from './components/navbar'
+import Sidebra from './Sidebra'
 
 function App() {
   return (
     <>
+      <div>
+        <Navbar />
+        <Sidebra />
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
