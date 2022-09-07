df28932454afa037b8ec5fd4141dbbe2ef4a650f
armod <armods@gmail.com>
2022-09-07 17:09:23 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import './App.css'
 import Navbar from './components/navbar'
-import Sidebra from './Sidebra'
 
 function App() {
   return (
     <>
       <div>
         <Navbar />
-        <Sidebra />
       </div>
     </>
   )
